ab763106959e3c4828d92a1f0f99ce45497b5a6e
Muhammed <muhammedopeyemi5@gmail.com>
2020-08-30 09:21:43 +0100

    Sticky navbar

--- js/main.js
+++ js/main.js
@@ -1,11 +1,27 @@
-  // Sticky menu background
-  window.addEventListener('scroll', function() {
-    if (window.scrollY > 300) {
-      document.querySelector('#navbar').style.opacity = 0.9;
-      document.querySelector('#navbar').style.backgroundColor = "green";
+  // // Sticky menu background
+  // window.addEventListener('scroll', function() {
+  //   if (window.scrollY > 300) {
+  //     document.querySelector('#navbar').style.opacity = 0.9;
+  //     document.querySelector('#navbar').style.backgroundColor = "green";
+  //     document.querySelector('#navbar').style.position = "sticky";
       
-    } else {
-      document.querySelector('#navbar').style.opacity = 1;
-      document.querySelector('#navbar').style.backgroundColor = "transparent";
-    }
+  //   } else {
+  //     document.querySelector('#navbar').style.opacity = 1;
+  //     document.querySelector('#navbar').style.backgroundColor = "transparent";
+  //   }
+  // });
+  var navTop = $("#navbar").offset().top;
+console.log(navTop)
+  var stickyNav = function(){
+      if ($(window).scrollTop() > 150){
+          $("#navbar").addClass("sticky");
+      } else {
+          $("#navbar").removeClass("sticky");
+      }
+  };
+  
+  stickyNav();
+  
+  $(window).scroll(function(){
+      stickyNav();
   });
